1afafaf4e2d2c7587ea7cb1451e88f6342eb5162
zhao yang <yangzwj@gmail.com>
2016-04-01 01:17:04 +0800

    add thrift

---
+++ Assets/Scripts/MainTainService.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using Thrift;
+using Thrift.Transport;
+using Thrift.Protocol;
+using Generate;
+using System;
+
+public class MainTainService : MonoBehaviour {
+
+    private TTransport transport = null;
+    public string serverAddress = "localhost";
+    public int serverPort = 7911;
+    public bool autoConnect = true;
+    private UserStorage.Client client;
+    private TProtocol protocol;
+    public long sessionId = 0;
+	// Use this for initialization
+	void Start () {
+        if (autoConnect)
+        {
+            Connect();
+        }
+	}
+	
+	public void Connect()
+    {
+        try
+        {
+            transport = new TFramedTransport(new TSocket(serverAddress, serverPort));
+            protocol = new TBinaryProtocol(transport);
+            TMultiplexedProtocol tmp = new TMultiplexedProtocol(protocol, "GameService");
+            client = new UserStorage.Client(tmp);
+            transport.Open();
+            //phrase
+        }catch(TApplicationException x)
+        {
+            Debug.LogWarning(x.StackTrace);
+        }catch(Exception x)
+        {
+            Debug.LogWarning(x.StackTrace);
+        }
+    }
+
+    public UserStorage.Client GetClient()
+    {
+        return this.client;
+    }
+
+    public void Disconnect()
+    {
+        if(transport != null)
+        {
+            transport.Close();
+        }
+    }
+
+    public long SessionGenerate()
+    {
+        return client.generateSessionId();
+    }
+}
---
+++ Assets/Scripts/TestSuites/TestMaintain.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMaintain : MonoBehaviour {
+
+    MainTainService service;
+
+    void Awake()
+    {
+        service = GetComponent<MainTainService>();
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            InitToken();
+        }
+	}
+
+    public void InitToken()
+    {
+        long id = service.SessionGenerate();
+        Debug.Log(id);
+    }
+}
